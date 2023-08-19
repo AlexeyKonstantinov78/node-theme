afe805a70eb66d36dd6e5d4b2e3949aa22bd888d
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-19 20:50:37 +0300

    для созданние пакета что бы не указывать раширения при экпорте

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import mpackage from 'mpackage';
 
 if (mpackage.name === 'pow') {
-  const { pow } = await import('mpackage/pow.js');
+  const { pow } = await import('mpackage/pow');
   console.log(pow(2, 3));
 } else {
   console.log('mpackage: ', mpackage);
