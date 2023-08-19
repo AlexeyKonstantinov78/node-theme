38d82639de03eadccebc8765f29cb7a0f28d7b63
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-19 20:59:00 +0300

    используем оба варианта

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const mpackage = require('mpackage');
-const { pow } = require('mpackage/pow');
 
-if (mpackage.name === 'pow') {
-  console.log(pow(2, 3));
-} else {
   console.log('mpackage: ', mpackage);
-}
+
--- index.mjs
+++ index.mjs
@@ -1,10 +1,3 @@
 import mpackage from 'mpackage';
 
-if (mpackage.name === 'pow') {
-  const { pow } = await import('mpackage/pow');
-  console.log(pow(2, 3));
-} else {
-  console.log('mpackage: ', mpackage);
-}
-
-
+console.log('mpackage: ', mpackage);
