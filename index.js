5cf5c78d9c817146681174ffef7a4f2fe830d0d2
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-19 20:45:09 +0300

    etnascript

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mpackage = require('mpackage');
-const { pow } = require('mpackage/pow');
+const { pow } = require('mpackage/pow.mjs');
 
 if (mpackage.name === 'pow') {
   console.log(pow(2, 3));
--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import mpackage from 'mpackage';
 
 if (mpackage.name === 'pow') {
-  const { pow } = await import('mpackage/pow.js');
+  const { pow } = await import('mpackage/pow.mjs');
   console.log(pow(2, 3));
 } else {
   console.log('mpackage: ', mpackage);
