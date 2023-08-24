98fa1e451f20a49463b8806db4607023875e3c85
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-24 17:26:01 +0300

    feat add fun

---
+++ index.js
@@ -0,0 +1,63 @@
+// события  Event Emmitter (патерн)
+import { EventEmitter } from 'node:events';
+
+class Ee extends EventEmitter { };
+
+const ee = new Ee();
+
+// ограниечение по количеству событий по умолчанию 10
+//ee.setMaxListeners(1);
+console.log(ee.getMaxListeners());
+
+const logger = (...arg) => {
+  console.log('logger arg:', arg);
+};
+
+
+
+//напишем свое событие
+ee.addListener('foo', (x) => {
+  console.log('addListener!', x);
+});
+
+// тоже самое что addListner
+ee.on('foo', logger);
+
+ee.on('bar', (x) => {
+  console.log('on! bar', x);
+});
+
+// событие once срабатывает один раз
+ee.once('foo', (x) => {
+  console.log('once!', x);
+});
+
+// запуск листнера с логированием после самомго события
+ee.prependListener('foo', (...args) => {
+  console.log('foo loger', 'args: ', args);
+});
+// изменение вызова обекта события
+ee.prependListener('foo', (x) => {
+  x.cia = 'шпион';
+});
+
+// инициалихзация события
+ee.emit('foo', { a: 1 });
+ee.emit('foo', { b: 1 });
+ee.emit('foo', { c: 1 });
+ee.emit('bar', { c: 1 });
+//ee.removeAllListeners(); // удаление всех обработчиков
+//ee.removeAllListeners('bar'); // удаление одного обработчиков
+ee.removeListener('foo', logger); // удаление с функцией logger
+ee.off('foo', logger);
+// удаление с функцией logger ee.removeListener('foo', logger) тоже самое
+ee.emit('foo', { a: 666 });
+ee.emit('foo', { b: 777 });
+ee.emit('foo', { c: 888 });
+ee.emit('bar', { c: 999 });
+
+// сколько листнеров навешано
+console.log(ee.listenerCount('foo'));
+console.log(ee.listeners());
+
+
