b4b224105b8e226afa4aca4188deb258ebcb2280
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2023-08-26 21:11:21 +0300

    просмотр директории

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 import fs from 'node:fs';
 //эти методы являются синхоронными
 //чтение файла
-const result = fs.readFileSync('./files/text.txt', 'utf8');
-// кодировка нужна толшько при работе в коде при записи кодировка не нужна
-console.log('result: ', result);
+// const result = fs.readFileSync('./files/text.txt', 'utf8');
+// // кодировка нужна толшько при работе в коде при записи кодировка не нужна
+// console.log('result: ', result);
 
-// запись в файл
-fs.writeFileSync('./files/result.txt', result);
+// // запись в файл
+// fs.writeFileSync('./files/result.txt', result);
 
-// асинхронные методы
-fs.readFile('./files/text.txt', 'utf-8', (err, result) => {
-  if (err) throw err;
+// // асинхронные методы
+// fs.readFile('./files/text.txt', 'utf-8', (err, result) => {
+//   if (err) throw err;
 
-  console.log(result);
-  fs.writeFile('./files/result1.txt', result, err => {
-    if (err) throw err;
+//   console.log(result);
+//   fs.writeFile('./files/result1.txt', result, err => {
+//     if (err) throw err;
 
-    console.log('Файл был записан');
-  });
-});
+//     console.log('Файл был записан');
+//   });
+// });
 
-//переименовывать или перемещать
-fs.rename('./files/text.txt', './files/result3.txt', err => {
-  if (err) throw err;
-  console.log('Файл переименован');
-});
+// //переименовывать или перемещать
+// fs.rename('./files/text.txt', './files/result3.txt', err => {
+//   if (err) throw err;
+//   console.log('Файл переименован');
+// });
 
 // прочитать каталог
 fs.readdir('./files', (err, files) => {
+  if (err) throw err;
+  console.log(files);
+});
+fs.readdir('./', (err, files) => {
   if (err) throw err;
   console.log(files);
 });
