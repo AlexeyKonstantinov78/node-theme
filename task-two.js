fa8d71e6fa5112b3222320d7f4e2878d63b52200
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-26 09:41:54 +0300

    update task two

--- task-two.js
+++ task-two.js
@@ -1,46 +1,30 @@
 import { EventEmitter } from 'node:events';
 
 //task two
-
-let name = '';
-let messag = '';
-
 class Mess extends EventEmitter {
   constructor() {
     super();
     this.name = '';
     this.mess = '';
+    this.on('send', this.receiveMessage);
   }
 
   sendMessage(username, message) {
     this.name = username;
     this.mess = message;
+    this.emit('send');
   }
 
   receiveMessage() {
-    console.log(name + ': ' + messag);
+    console.log(this.name + ': ' + this.mess);
   }
 
   emit(name, ...args) {
     super.emit(name, ...args);
-
   }
 }
 
 const mess = new Mess();
 
-const sendMessage = (username, message) => {
-  name = username;
-  messag = message;
-};
-
-const receiveMessage = () => {
-  console.log(name + ': ' + messag);
-};
-
-mess.on('send', sendMessage);
-mess.on('send', receiveMessage);
-
-mess.emit('send', 'maks', 'привет');
-mess.emit('send', 'Lex', 'привет');
-mess.emit('send', 'maks', 'сам с приветом');
+mess.sendMessage('maks', 'привет');
+mess.sendMessage('Lex', 'привет');
