37d19ce11428b94e0446b39bfb30d442a559f733
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-27 18:33:38 +0300

    получение данных о файле

---
+++ index2.js
@@ -0,0 +1,14 @@
+import { stat } from 'node:fs';
+import fs from 'node:fs/promises';
+
+const checkFileStats = async path => {
+  try {
+    const stats = await fs.stat(path);
+
+    console.log(stats);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+checkFileStats('./files/text.txt');
