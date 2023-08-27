b8a47821c4fd3d589f5f499c7f94f1098d23750c
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-27 20:49:51 +0300

    var 1.0..0

--- task-one.js
+++ task-one.js
@@ -4,20 +4,25 @@ const sourceDir = './files';
 const targetDir = './newFolder';
 
 const callback = (err) => {
-  if (err !== null) console.log(err);
+  if (err !== null) {
+    console.log(err);
+  } else {
+    console.log('Завершено');
+  }
 };
 
-const copyAllDir = async (sourceDir, targetDir, callback) => {
+const copyAllDir = async (sourceDir, targetDir, callback = () => {}) => {
   try {
     if ((await stat(sourceDir)).isDirectory()) {
       const files = await readdir(sourceDir);
       mkdir(targetDir, { recursive: true });
       files.forEach(async file => {
-        await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`);
+        await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`, callback(null));
       });
     } else if ((await stat(sourceDir)).isFile()) {
       await copyFile(sourceDir, targetDir);
     }
+    if (callback !== null) callback(null);
   } catch (error) {
     callback(error.message);
   }
