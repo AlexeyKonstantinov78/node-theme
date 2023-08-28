b85f6a0b2f1eda10da83feb6478003ea3191dc01
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-28 16:02:30 +0300

    task

--- task-two.js
+++ task-two.js
@@ -1,10 +1,65 @@
 import { EventEmitter } from 'node:events';
+import { appendFile, stat, truncate, copyFile } from 'node:fs/promises';
 class Logger extends EventEmitter {
   constructor(filename, maxSize) {
     super();
     this.filename = filename;
     this.maxSize = maxSize;
     this.logQueue = [];
-    this.writing = true;
+    this.writing = false;
+  }
+
+  log(message) {
+    this.logQueue.unshift(message);
+    this.writeLog();
+    if (!this.writing) this.writing = true;
+  }
+
+  writeLog() {
+    this.logQueue.forEach(async log => {
+      await appendFile(this.filename, `${log}\n`);
+
+    });
+    this.emit('messageLogged', this.logQueue);
+    this.logQueue = [];
+    if (this.logQueue.length > 0)
+      this.writeLog();
+    else
+      this.writing = false;
+
+    this.checkFileSize();
+  }
+
+  async checkFileSize() {
+    if ((await this.getFileSize()) > this.maxSize) {
+      this.rotateLog();
+    }
+  }
+
+  async getFileSize() {
+    try {
+      return (await stat(this.filename)).size;
+    } catch (error) {
+      return 0;
+    }
+  }
+
+  async rotateLog() {
+    try {
+      await copyFile(this.filename, `${this.filename}.bak`);
+      await truncate(this.filename, this.maxSize);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 }
+
+const logger = new Logger('log.txt', 1024);
+
+logger.on('messageLogged', message => {
+  console.log('Записано сообщение:', message);
+});
+
+logger.log('Первое сообщение1');
+
+logger.log('Второе сообщение2');
