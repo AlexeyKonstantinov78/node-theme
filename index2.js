6054b78be88cd1d5d828f9533335c04c95fe73ed
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-30 18:31:59 +0300

    Writable stream

--- index2.js
+++ index2.js
@@ -1,38 +1,69 @@
-import fs from 'node:fs/promises';
+// ? Stream
 
-const checkFileStats = async path => {
-  try {
-    const stats = await fs.stat(path);
+// ! Writable
+// работает в fs, http, net, resposn у сервера, request на сторне клиента, zlib для архивации файлов,
+// proccess, cripto, tsp socet
 
-    console.log(stats);
+import { createReadStream, createWriteStream } from 'node:fs';
 
-    // проверка на тип файл или каатлог
-    if (stats.isFile()) {
-      console.log('Файл');
-    } else if (stats.isDirectory()) {
-      console.log('Директория');
-    } else {
-      console.log('Это неизвестный тип');
-    }
+const wStream = createWriteStream('./write.txt'); // запись файл ,elbn cjplfy
+const rStream = createReadStream('./files/text.txt'); // чтение файла
 
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+wStream.on('pipe', () => {
+  console.log('PIPE - подключение к readeable стриму');
+});
 
-checkFileStats('./files/text.txt');
+wStream.on('unpipe', () => {
+  console.log('UNPIPE - отключение от readeable стриму');
+});
 
-// дописываем в файл
+wStream.on('finish', () => {
+  console.log('FINISH - запись ЗАВЕРШИЛОСЬ');
+});
 
-const appendToFile = async (filePath, data) => {
-  try {
-    await fs.appendFile(filePath, data);
-    console.log('Запись дописана');
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+// срабатывает когда буфер заполнился и освободился
+wStream.on('drain', () => {
+  console.log('DRAIN - освободился буфер у writable');
+});
 
-appendToFile(
-  './files/text.txt',
-  `${new Date().toISOString()}: Допишем текст 1\n`);
+wStream.on('error', (err) => {
+  console.log('ERROR - ' + err);
+});
+
+wStream.on('close', () => {
+  console.log('CLOSE - стрим закрыт');
+});
+
+// wStream.write('Запись данных1\n', 'utf-8', () => {
+//   console.log('Запись данных');
+// });
+// const buffer = Buffer.from('Буфер\n');
+// wStream.write(buffer);
+// wStream.write(buffer);
+// wStream.write(buffer);
+// wStream.write(buffer);
+// wStream.write(buffer);
+
+// wStream.end('Закрываем стрим'); // можно передавать можно не писать
+
+// wStream.destroy('Наша ошибка'); // грубое закрытие стрима но приэтом не записываются и выводим ошибку
+
+// wStream.cork(); // закупорить стрим
+
+// setTimeout(() => {
+//   wStream.uncork(); // раскупорить стрим
+// }, 2000);
+
+wStream.setDefaultEncoding('utf-8'); // при работе с текстом
+console.log('writableCorked: ', wStream.writableCorked); // указывает количество закупорок
+console.log('errored: ', wStream.errored); // сколько было ошибок
+console.log('writableLength', wStream.writableLength); // байт ав байтах
+console.log('writableHighWaterMark', wStream.writableHighWaterMark); // занимаемое буфером память
+
+// ! Readable
+
+rStream.pipe(wStream); // чтение -> запись в файл
+
+// ! Duplex
+
+// !Transform
