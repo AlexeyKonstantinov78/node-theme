b1017142e90bf5336b9b44c8cd5d06444318ed58
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-08-30 14:41:53 +0300

    безопасный и не безопасный буфер

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ import { write } from './modules/writeFileCustom.js';
 
 const app = async () => {
   try {
-    const data = await read('./files/gimn.mp3');
-    console.log('data: ', data); // toString('utf8') лучше указывать кодировку
-    await write('./files/rezul.mp3', data);
+    // const data = await read('./files/text.txt');
+    // работа с буфером буфер это абтрактное хранилище данных 16 ричном виде данных
+    //console.log('data: ', data); // toString('utf8') лучше указывать кодировку
+
+    // создаем буфер не безопасный
+    const bufferUnsafe = Buffer.allocUnsafe(1024); // работает быстрее но не безопасно
+    console.log('bufferUnsafe: ', bufferUnsafe);
+
+    // безопасный буфер
+    const bufferSafe = Buffer.alloc(1024, 'изучаем node');
+    console.log('bufferSafe: ', bufferSafe);
+
+
+    //await write('./files/rezul.txt', data);
     console.log('Запиь в файл записан');
   } catch (error) {
     console.error(error.message);
