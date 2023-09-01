82c6fe87ac191488edf1280e63cf0e4793bd07c3
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-01 16:40:14 +0300

    ver 1.2.0

--- task-two.js
+++ task-two.js
@@ -1,5 +1,9 @@
 import { createReadStream, createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
+import { readdir, mkdir, stat } from 'node:fs/promises';
+
+const sourceDir = './files';
+const targetFile = './write.txt';
 
 const copyFiles = async (path, nameFile) => {
   try {
@@ -17,4 +21,24 @@ const copyFiles = async (path, nameFile) => {
   }
 };
 
-copyFiles('./filesdir', './write.txt');
+//copyFiles('./filesdir', './write.txt');
+
+const copyAllDir = async (sourceDir, targetDir, targetFile) => {
+  try {
+    if ((await stat(sourceDir)).isDirectory()) {
+      const files = await readdir(sourceDir);
+      mkdir(targetDir, { recursive: true });
+      console.log(`dir ${sourceDir}`);
+      files.forEach(async file => {
+        await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`);
+      });
+    } else if ((await stat(sourceDir)).isFile()) {
+      await copyFiles(sourceDir, targetFile);
+      console.log('file', sourceDir);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+copyAllDir(sourceDir, './newFolders', targetFile);
