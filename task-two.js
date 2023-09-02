544e6b3629000f74e525a750b71345de6e9afa9c
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-02 09:55:33 +0300

    ver 2.0.0

--- task-two.js
+++ task-two.js
@@ -1,5 +1,4 @@
 import { createReadStream, createWriteStream } from 'node:fs';
-import { pipeline } from 'node:stream/promises';
 import { readdir, stat } from 'node:fs/promises';
 
 const sourceDir = './filesdir';
@@ -8,12 +7,21 @@ const targetFile = './write.txt';
 const readFileDir = async (sourceDir, targetFile) => {
   try {
     const wStream = createWriteStream(targetFile);
+    wStream.setDefaultEncoding('utf-8');
 
     if ((await stat(sourceDir)).isDirectory()) {
       const files = await readdir(sourceDir);
 
-      files.forEach(file => {
+      files.forEach(async file => {
         console.log(file);
+        const buffFile = Buffer.from(file);
+        const stream = createReadStream(`${sourceDir}/${file}`);
+        if (buffFile.includes('.txt')) {
+          wStream.write(Buffer.from(`[${file}]\n`));
+          for await (const chunk of stream) {
+            wStream.write(`${chunk}`);
+          }
+        }
       });
     }
   } catch (error) {
@@ -22,44 +30,3 @@ const readFileDir = async (sourceDir, targetFile) => {
 };
 
 readFileDir(sourceDir, targetFile);
-
-// const rStream = createReadStream();
-// rStream.write(Buffer.from('ghbdtn'));
-// console.log(rStream);
-
-// const copyFiles = async (path, nameFile) => {
-//   console.log('path, nameFile: ', path, nameFile);
-//   try {
-//     await pipeline(
-//       createReadStream(path),
-//       // либо еще операции
-//       createWriteStream(nameFile),
-//     );
-
-//     console.log('ready');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-// //copyFiles('./filesdir', './write.txt');
-
-// const copyAllDir = async (sourceDir, targetDir, targetFile) => {
-//   try {
-//     if ((await stat(sourceDir)).isDirectory()) {
-//       const files = await readdir(sourceDir);
-//       mkdir(targetDir, { recursive: true });
-//       console.log(`dir ${sourceDir}`);
-//       files.forEach(async file => {
-//         await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`);
-//       });
-//     } else if ((await stat(sourceDir)).isFile()) {
-//       await copyFiles(sourceDir, targetFile);
-//       console.log('file', sourceDir);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// copyAllDir(sourceDir, './newFolders', targetFile);
