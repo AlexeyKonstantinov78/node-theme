c1343051d1b8c172bf66c09bd1b8b65cd8ee1ba4
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-02 09:21:12 +0300

    ver 1.3.0

--- task-two.js
+++ task-two.js
@@ -1,44 +1,65 @@
 import { createReadStream, createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
-import { readdir, mkdir, stat } from 'node:fs/promises';
+import { readdir, stat } from 'node:fs/promises';
 
-const sourceDir = './files';
+const sourceDir = './filesdir';
 const targetFile = './write.txt';
 
-const copyFiles = async (path, nameFile) => {
+const readFileDir = async (sourceDir, targetFile) => {
   try {
-    await pipeline(
-      createReadStream(path),
+    const wStream = createWriteStream(targetFile);
 
-      // либо еще операции
-
-      createWriteStream(nameFile),
-    );
-
-    console.log('ready');
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-//copyFiles('./filesdir', './write.txt');
-
-const copyAllDir = async (sourceDir, targetDir, targetFile) => {
-  try {
     if ((await stat(sourceDir)).isDirectory()) {
       const files = await readdir(sourceDir);
-      mkdir(targetDir, { recursive: true });
-      console.log(`dir ${sourceDir}`);
-      files.forEach(async file => {
-        await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`);
+
+      files.forEach(file => {
+        console.log(file);
       });
-    } else if ((await stat(sourceDir)).isFile()) {
-      await copyFiles(sourceDir, targetFile);
-      console.log('file', sourceDir);
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-copyAllDir(sourceDir, './newFolders', targetFile);
+readFileDir(sourceDir, targetFile);
+
+// const rStream = createReadStream();
+// rStream.write(Buffer.from('ghbdtn'));
+// console.log(rStream);
+
+// const copyFiles = async (path, nameFile) => {
+//   console.log('path, nameFile: ', path, nameFile);
+//   try {
+//     await pipeline(
+//       createReadStream(path),
+//       // либо еще операции
+//       createWriteStream(nameFile),
+//     );
+
+//     console.log('ready');
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// //copyFiles('./filesdir', './write.txt');
+
+// const copyAllDir = async (sourceDir, targetDir, targetFile) => {
+//   try {
+//     if ((await stat(sourceDir)).isDirectory()) {
+//       const files = await readdir(sourceDir);
+//       mkdir(targetDir, { recursive: true });
+//       console.log(`dir ${sourceDir}`);
+//       files.forEach(async file => {
+//         await copyAllDir(`${sourceDir}/${file}`, `${targetDir}/${file}`);
+//       });
+//     } else if ((await stat(sourceDir)).isFile()) {
+//       await copyFiles(sourceDir, targetFile);
+//       console.log('file', sourceDir);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// copyAllDir(sourceDir, './newFolders', targetFile);
