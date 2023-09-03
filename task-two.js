2fec0e97de56828cc53cde82eb442f7071a1d430
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-03 14:40:22 +0300

    u

--- task-two.js
+++ task-two.js
@@ -4,6 +4,15 @@ import { readdir, stat } from 'node:fs/promises';
 const sourceDir = './filesdir';
 const targetFile = './write.txt';
 
+const readStream = async (rStream, wStream) => {
+  //const stream = createReadStream(path);
+
+  for await (const chunk of rStream) {
+    wStream.write(`${chunk}`);
+  }
+};
+
+
 const readFileDir = async (sourceDir, targetFile) => {
   try {
     const wStream = createWriteStream(targetFile);
@@ -15,17 +24,54 @@ const readFileDir = async (sourceDir, targetFile) => {
       files.forEach(async file => {
         console.log(file);
         const buffFile = Buffer.from(file);
-        const stream = createReadStream(`${sourceDir}/${file}`);
+
         if (buffFile.includes('.txt')) {
-          // wStream.write(`[${file}]\n`);
+          const stream = createReadStream(`${sourceDir}/${file}`, 'utf-8');
+          wStream.write(`[${file}]\n`);
+          // if (!stream.isPaused) {
+          //   console.log('pausa');
+          //   wStream.cork();
+          // }
+          // //stream.pipe(wStream);
+          // stream.pause();
+          readStream(stream, wStream);
+          // for await (const chunk of stream) {
+          //   wStream.write(`${chunk}`);
+          // }
+
+          //stream.on('data', chunk => wStream.write(chunk.toString()));
           // stream.pipe(wStream);
-          for await (const chunk of stream) {
-            wStream.write(Buffer.from(`[${file}]\n`));
-            wStream.write(`${chunk}`);
-          }
+          // for await (const chunk of stream) {
+          //   // wStream.write(Buffer.from(`[${file}]\n`));
+          //   wStream.write(`[${file}]\n`);
+          //   wStream.write(`${chunk}`);
+          // }
+          // wStream.cork();
+
+          // stream.pause();
+
+          // stream.on('readable', () => {
+          //   const buffer = stream.read();
+          //   if (buffer) {
+          //     wStream.write(buffer);
+          //   }
+          //   wStream.uncork();
+          // });
+
+          // stream.on('end', () => {
+          //   console.log('END');
+          //   stream.resume();
+          // });
+          // stream.on('data', chunk => {
+          //   wStream.write(`[${file}]\n`);
+          //   wStream.write(chunk);
+          // });
         }
       });
     }
+    wStream.on('finish', () => {
+      console.log('finish');
+    });
   } catch (error) {
     console.log(error);
   }
