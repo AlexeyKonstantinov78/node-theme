34e95625056f97f5985af60ba7b42b0898380353
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-04 09:11:03 +0300

    update

--- task-three.js
+++ task-three.js
@@ -1,23 +1,55 @@
 import sharp from 'sharp';
 import { readdir, stat } from 'node:fs/promises';
 import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 
 const inputPath = './image';
 const outputPath = './imagenew';
 
-const blurGreyscaleImage = async (inputPath, outputPath) => {
+const resizeImage = async (inputPath, outputPath) =>
+  await pipeline(
+    createReadStream(inputPath),
+    sharp().resize(400, 400).jpeg(),
+    createWriteStream(outputPath),
+  );
+
+const blurImage = async (inputPath, outputPath, value = 0) =>
+  await pipeline(
+    createReadStream(inputPath),
+    sharp().blur(value).jpeg(),
+    createWriteStream(outputPath),
+  );
+
+const greyscaleImage = async (inputPath, outputPath) =>
+  await pipeline(
+    createReadStream(inputPath),
+    sharp().greyscale().jpeg(),
+    createWriteStream(outputPath),
+  );
+
+const Image = async (inputPath, outputPath) => {
 
   try {
     if ((await stat(inputPath)).isDirectory()) {
       const files = await readdir(inputPath);
 
       files.forEach(async file => {
-        const rStream = createReadStream(`${inputPath}/${file}`);
-        const wStream = createWriteStream(`${outputPath}/${file + Date.now()}.jpeg`);
-        const roundedCornerResizer = await sharp().blur(5).greyscale().jpeg();
         console.log(file);
+        resizeImage(
+          `${inputPath}/${file}`,
+          `${outputPath}/${file + Date.now()}.jpeg`,
+        );
 
-        rStream.pipe(roundedCornerResizer).pipe(wStream);
+        blurImage(
+          `${inputPath}/${file}`,
+          `${outputPath}/${file + Date.now()}.jpeg`,
+          5
+        );
+        greyscaleImage(
+          `${inputPath}/${file}`,
+          `${outputPath}/${file + Date.now()}.jpeg`,
+        );
       });
     }
   } catch (error) {
@@ -25,25 +57,4 @@ const blurGreyscaleImage = async (inputPath, outputPath) => {
   }
 };
 
-const resizeImage = async (inputPath, outputPath) => {
-
-  try {
-    if ((await stat(inputPath)).isDirectory()) {
-      const files = await readdir(inputPath);
-
-      files.forEach(async file => {
-        const rStream = createReadStream(`${inputPath}/${file}`);
-        const wStream = createWriteStream(`${outputPath}/${file}.jpeg`);
-        const roundedCornerResizer = sharp().resize(400, 400).jpeg();
-        console.log(file);
-
-        rStream.pipe(roundedCornerResizer).pipe(wStream);
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-resizeImage(inputPath, outputPath);
-blurGreyscaleImage(inputPath, outputPath);
+Image(inputPath, outputPath);
