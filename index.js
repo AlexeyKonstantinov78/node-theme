6dc326eff98b2f57289e7c783c71ef9138ab863b
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-05 16:57:44 +0300

    process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import readline from 'node:readline/promises';
+import process from 'node:process';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -14,7 +15,10 @@ rl.prompt();
 
 rl.on('line', answer => {
   console.log(`Hello, ${answer}`);
-  rl.prompt();
+  rl.close();
+  //rl.prompt();
 });
 
 //rl.close(); // завершает readline
+//console.dir({ process });
+
