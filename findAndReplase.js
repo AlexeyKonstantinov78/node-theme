296251f22b93cd875606108425ef0e292cc56108
AlexeyKonstantinov78 <alex_kav@mail.ru>
2023-09-11 09:16:37 +0300

    find and replace final

--- findAndReplase.js
+++ findAndReplase.js
@@ -8,6 +8,23 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const read = patch => {
+  try {
+    const data = fs.readFileSync(patch, 'utf8');
+    return data;
+    // return data.split('\n').filter(n => n);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const write = (data, patch) => {
+  if (!fs.existsSync(path.dirname(patch))) {
+    fs.mkdirSync(path.dirname(patch));
+  }
+  fs.writeFileSync(patch, data);
+};
+
 const quest = async () => {
   const findDir = await rl.question('Введите путь к директории: ');
   const search = await rl.question('Введите строка поиска: ');
@@ -24,10 +41,21 @@ if (!fs.existsSync(data.findDir)) {
 
 const fileDir = fs.readdirSync(data.findDir);
 
+const replaseData = (data, search, replase) => {
+  let newdata = data;
+  do {
+    newdata = newdata.replace(search, replase);
+  } while (newdata.indexOf(search) !== -1);
+
+  return newdata;
+};
+
 fileDir.forEach(file => {
   const filePath = path.join(data.findDir, file);
   if (path.extname(filePath) === '.txt') {
-
+    const dataFile = read(filePath);
+    const newData = replaseData(dataFile, data.search, data.replace);
+    write(newData, filePath);
   }
 });
 
