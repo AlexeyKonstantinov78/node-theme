4c8041c62de5f1d39c2489582d5f332467c050c2
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-09-27 21:08:51 +0300

    add gzip arx

--- services/hex.services.js
+++ services/hex.services.js
@@ -1,18 +1,26 @@
 import crypto from 'node:crypto';
 import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
 
 export const hexfile = async file => {
 
   const __dirname = path.dirname(file);
   const namefile = path.basename(file);
-  //const extname = path.extname(file);
 
   const writeNameFile = path.join(__dirname, namefile.replace('.', '_') + '.sha256');
 
   const dataFile = await readFile(file);
 
   const hashFile = crypto.createHash('sha256').update(dataFile).digest('hex');
-  //console.log('hashFile: ', hashFile);
   await writeFile(writeNameFile, hashFile);
+
+  const outputFilePath = path.join(__dirname, namefile.split('.')[0] + '.gz');
+
+  const input = createReadStream(file);
+  const output = createWriteStream(outputFilePath);
+  input.pipe(createGzip()).pipe(output);
+
+  console.log('Данные успешно упакованны');
 };
