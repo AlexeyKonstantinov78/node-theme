078220b3e4445f73b13095f56f3b82acd6d088d0
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-09-28 13:47:46 +0300

    update hash

--- util/hashFile.js
+++ util/hashFile.js
@@ -4,7 +4,6 @@ import { readFile } from 'node:fs/promises';
 export const hashFile = async file => {
 
   const dataFile = await readFile(file);
-  console.log('dataFile: ', dataFile);
 
   const hash = crypto.createHash('sha256').update(dataFile).digest('hex');
 
