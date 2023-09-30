b77779ed43403f1237da8ef7313e0eb0cac1109f
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2023-09-30 22:58:05 +0300

    поиск через параметр

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ const client = new pg.Client({
 await client.connect();
 
 try {
+  const str = process.argv[2];
   //const res = await client.query('SELECT * FROM users WHERE id = 1');
-  const res = await client.query("SELECT * FROM users WHERE name = 'alex'");
+  const res = await client.query(`SELECT * FROM users WHERE name = '${str}'`);
   console.log('res: ', res.rows);
 } catch (error) {
   console.error(error.message);
