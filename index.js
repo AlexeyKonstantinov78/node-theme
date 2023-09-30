84668772994891d6559e85b9b492ae89e3993a35
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2023-09-30 23:47:41 +0300

    поиск по имени

--- index.js
+++ index.js
@@ -50,14 +50,18 @@ const deleteUser = async id => {
   await client('users').where({ id }).delete();
 };
 
+const getUserByName = async name =>
+  await client('users').where('name', '=', name);
+
 const init = async () => {
   //await addUser('Vovan', '89002225544', 'vovan@vovan.ru', 55); // добавление
 
   // await updateUser(7, {
   //   name: 'Vovan2',
   // });
-  await deleteUser(6);
-  console.log(await getAllUsers());
+  // await deleteUser(6);
+  console.log(await getUserByName('alex'));
+  //console.log(await getAllUsers());
 
   client.destroy(); // закрываем соединение
 };
