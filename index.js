b8777ad0f0932a6d3b1c79b61e284040c9c06169
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2023-09-30 22:51:16 +0300

    подключение к бд выполнение ыйд запроса

---
+++ index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import pg from 'pg';
+
+const HOST = process.env.DB_HOST;
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+const DATABASE = process.env.DB_DATABASE;
+const PORT = process.env.DB_PORT;
+
+const client = new pg.Client({
+  host: HOST,
+  port: PORT || 5432,
+  database: DATABASE,
+  user: USER,
+  password: PASSWORD,
+});
+
+await client.connect();
+
+try {
+  const res = await client.query('SELECT * FROM users WHERE id = 1');
+  console.log('res: ', res.rows);
+} catch (error) {
+  console.error(error.message);
+}
+
+client.end();
