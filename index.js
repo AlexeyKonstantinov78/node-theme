5971173a92169d2bafe2e336a8b11d3f2985dff5
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2023-09-30 23:15:36 +0300

    получение через knex

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import pg from 'pg';
+//import pg from 'pg';
+import knex, { default as Knex } from 'knex';
 
 const HOST = process.env.DB_HOST;
 const USER = process.env.DB_USER;
@@ -7,23 +8,31 @@ const PASSWORD = process.env.DB_PASSWORD;
 const DATABASE = process.env.DB_DATABASE;
 const PORT = process.env.DB_PORT;
 
-const client = new pg.Client({
-  host: HOST,
-  port: PORT || 5432,
-  database: DATABASE,
-  user: USER,
-  password: PASSWORD,
+const client = Knex({
+  client: 'pg',
+  connection: {
+    host: HOST,
+    port: PORT || 5432,
+    database: DATABASE,
+    user: USER,
+    password: PASSWORD,
+  },
 });
 
-await client.connect();
+// await client.connect();
 
-try {
-  const str = process.argv[2];
-  //const res = await client.query('SELECT * FROM users WHERE id = 1');
-  const res = await client.query('SELECT * FROM users WHERE name = $1', [str]);
-  console.log('res: ', res.rows);
-} catch (error) {
-  console.error(error.message);
-}
+// try {
+//   const str = process.argv[2];
+//   //const res = await client.query('SELECT * FROM users WHERE id = 1');
+//   const res = await client.query('SELECT * FROM users WHERE name = $1', [str]);
+//   console.log('res: ', res.rows);
+// } catch (error) {
+//   console.error(error.message);
+// }
 
-client.end();
+// client.end();
+
+
+
+const users = await client('users');
+console.log('users: ', users);
