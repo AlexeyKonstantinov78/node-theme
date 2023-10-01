3dcf7cc09a66351bb548ad605515d2f0d1f74c7e
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2023-10-01 18:20:48 +0300

    update add task

--- service/addTask.js
+++ service/addTask.js
@@ -1,19 +1,5 @@
-import { read, write } from '../util/readWriteFile.js';
+import { addTodoTask } from './todo.service.js';
 
-export const addTask = task => {
-  let data = read();
-  let count = 0;
-
-  if (data !== undefined) {
-    if (data.length > 0) {
-      count = data[data.length - 1].substring(0, data[data.length - 1].indexOf('.'));
-    }
-  } else {
-    data = [];
-  }
-
-  const id = parseInt(count) + 1;
-  data.push(`${id}. [В работе] ${task.join(' ')}`);
-  write(data);
-  console.log('Задача добавлена с идентификатором ' + id);
+export const addTask = async task => {
+  await addTodoTask('[В работе]', task.join(' '));
 };
